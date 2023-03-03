ea4f935336d1c21d1cef3776c834611f8bc577da
Segundo Mendez <mendezsegun@gmail.com>
2023-03-02 22:10:34 -0300

    Se agrega llamado al backend

---
+++ errormanager-front/src/backend api/api-controller.js
@@ -0,0 +1,31 @@
+
+
+export const existsError = async function(service, endpoint, id){
+    let url = "http://localhost:8080/error/exists-error/"+service+"/"+endpoint+"/"+id;
+    console.log("Consumiendo localhost...");
+    try{
+        let response = await fetch(
+            url,
+            {
+                method:'GET',
+                mode:'cors'
+            }
+        );
+        let rdo = response.status;
+        let datos = await response.json();
+        switch(rdo)
+        {
+                case 200:
+                    {
+                        console.log(datos.exists);
+                        return( {rdo:0, mensaje:"ok", existsError: datos.exists});
+                    }
+                default:
+                    {
+                        return({rdo:1, mensaje:"Ocurrio un error"});
+                    }
+        }
+    } catch(e){
+        console.log("Ocurrio un error al invocar getFeaturedPosts() ");
+    }
+}
--- errormanager-front/src/components/create/create-error-page.js
+++ errormanager-front/src/components/create/create-error-page.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import "../create/create-error-page.css";
+import { existsError } from "../../backend api/api-controller";
 
 class CreateErrorPage extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       service: "",
       endpoint: "",
-      id:"",
-      mensaje:"",
-      codigo:"",
+      id: "",
+      mensaje: "",
+      codigo: "",
       exists: true,
     };
 
@@ -19,94 +19,115 @@ class CreateErrorPage extends Component {
     this.handleFieldChange = this.handleFieldChange.bind(this);
   }
 
-  handleFieldChange(event){
+  handleFieldChange(event) {
     this.setState({
-        [event.target.name]:event.target.value
-    })
-}
-
-
-  async handleSubmitExistsId(event) {
-    console.log("Registro de entradas: "+ this.state.endpoint + " - "+ this.state.service  + " - "+ this.state.id);
-    this.setState({ exists : false });
-    console.log(this.state.exists);
-    /*
-    event.preventDefault();
-    fetch("", {
-      method: "POST",
-      headers: {
-        Accept: "Application/json",
-        "Content-Type": "Application/json",
-      },
-      body: JSON.stringify({
-        service: this.state.service,
-        endpoint: this.state.endpoint,
-        id: this.state.id,
-      }),
-    }).catch((err) => {
-      console.log("Ocurrio un error en la creacion del customer." + err);
+      [event.target.name]: event.target.value,
     });
-    */
+  }
+
+  async handleSubmitExistsId() {
+    console.log(
+      "Registro de entradas: " +
+        this.state.endpoint +
+        " - " +
+        this.state.service +
+        " - " +
+        this.state.id
+    );
+    const existsResponse = await existsError(
+      this.state.service,
+      this.state.endpoint,
+      this.state.id
+    );
+
+    console.log(existsResponse);
+    if (existsResponse.rdo === 0) {
+      if (!existsResponse.existsError) {
+        console.log(existsResponse.existsError);
+        this.setState({ exists : false })
+
+        //  No setea el estado en false. linea 47.
+
+        window.alert("xd ")
+
+      }else{
+        window.alert("El id ingresado ya se encuentra registrado!")
+      }
+    } else {
+      console.log("f");
+    }
   }
 
   render() {
-    if(this.state.exists){
+    if (this.state.exists) {
       return (
-          
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmitExistsId}>
           <div className="title">Crear nuevo mensaje de error</div>
           <div className="subtitle">Primero, validemos si ya existe:</div>
-  
+
           <div className="input-container ic1">
-            <input className="input" name="service" placeholder=" "  onChange={this.handleFieldChange}/>
+            <input
+              className="input"
+              name="service"
+              placeholder=" "
+              onChange={this.handleFieldChange}
+            />
             <div className="cut"></div>
-            <label className="placeholder">
-              Service
-            </label>
+            <label className="placeholder">Service</label>
           </div>
-  
+
           <div className="input-container ic2">
-            <input className="input" name="endpoint" placeholder=" "  onChange={this.handleFieldChange}/>
+            <input
+              className="input"
+              name="endpoint"
+              placeholder=" "
+              onChange={this.handleFieldChange}
+            />
             <div className="cut"></div>
-            <label className="placeholder">
-              Endpoint
-            </label>
+            <label className="placeholder">Endpoint</label>
           </div>
           <div className="input-container ic2">
-            <input className="input" name="id" placeholder=" " onChange={this.handleFieldChange}/>
+            <input
+              className="input"
+              name="id"
+              placeholder=" "
+              onChange={this.handleFieldChange}
+            />
             <div className="cut"></div>
-            <label className="placeholder">
-              Error id
-            </label>
+            <label className="placeholder">Error id</label>
           </div>
-          <button type="text" className="submit" onClick={this.handleSubmitExistsId}>
+          <button type="text" className="submit">
             Validar
           </button>
-          <button type="text" className="back" >
+          <button type="text" className="back">
             Volver
           </button>
         </form>
-        
       );
-
-    }else{
-      return(
-        <form  className="form">
+    } else {
+      return (
+        <form className="form">
           <div className="title">Crear nuevo mensaje de error</div>
           <div className="subtitle">Ingrese el mensaje y el codigo:</div>
           <div className="input-container ic1">
-            <input className="input" name="service" placeholder=" " onChange={this.handleFieldChange}/>
+            <input
+              className="input"
+              name="service"
+              placeholder=" "
+              onChange={this.handleFieldChange}
+            />
             <div className="cut"></div>
-            <label className="placeholder">
-              Mensaje de error:
-            </label>
+            <label className="placeholder">Mensaje:</label>
           </div>
           <div className="input-container ic1">
-            <input className="input" name="service" placeholder=" " onChange={this.handleFieldChange}/>
+            <input
+              className="input"
+              name="service"
+              placeholder=" "
+              onChange={this.handleFieldChange}
+            />
             <div className="cut"></div>
-            <label className="placeholder">
-              Codigo de error:
-            </label>
+            <label className="placeholder">Codigo:</label>
           </div>
           <button type="text" className="submit">
             Volver
